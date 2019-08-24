4c5457347b583d3ace0f96706d91660040d8ea0a
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-08-24 15:44:53 +0200

    remove cruft

--- init.lua
+++ init.lua
@@ -1,14 +1,3 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-	hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
-	local win = hs.window.focusedWindow()
-	local f = win:frame()
-	f.x = f.x - 10
-	win:setFrame(f)
-end)
-
 configFilePath = 'storedWindows.csv'
 scriptName = 'WindowKeeper'
 separator = ', '
